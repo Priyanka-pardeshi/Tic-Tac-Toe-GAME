442f801f6a717262042c88d5684b43802b041b22
echo # <priyankapardeshi2312@gmail.com>
2021-06-01 00:31:28 +0530

    Added an function to chec who start first

--- TicTacToeGame/TicTacToeGame/Program.cs
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -8,13 +8,27 @@ namespace TicTacToeGame
 {
     class Program
     {
+        public const int HEAD = 0;
+        public const int TAIL = 1;
+        public enum Player { USER, COMPUTER };
+
+        private static Player getWhoStartFirst()
+        {
+            int toss = getOneFromRandomChoice(2);
+            return (toss == HEAD) ? Player.USER : Player.COMPUTER;     
+        }
+        private static int getOneFromRandomChoice(int choices)
+        {
+            Random objRandom = new Random();
+            return (int)(objRandom.Next() * 10) % choices;
+        }
         static void Main(string[] args)
         {
            
             char[] board=TicTacToe.createBoard();                 //calling an method by usung class name
             Console.WriteLine(board);
             char choose = TicTacToe.chooseUserChar();
-            Console.WriteLine("Your choice is " + choose);
+            Console.WriteLine("Your choice is " + choose);       
             //Calling showBoard function
             TicTacToe.showBoard(board);
             //Gettin user Move
