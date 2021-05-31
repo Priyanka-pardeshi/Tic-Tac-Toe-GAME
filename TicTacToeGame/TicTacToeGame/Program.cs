264c0789b8654fb5c43bc7743a6f6c5b956491da
echo # <priyankapardeshi2312@gmail.com>
2021-05-31 15:30:59 +0530

    Created an board to Play Tic-Tac-Toe

---
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe objTicTac = new TicTacToe();         //Created an object of an class TicTacToe
+            objTicTac.createBoard();                      //accessing method by using class
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+        /// <summary>
+        /// Method to create an empty board 
+        /// </summary>
+        /// <returns>And returns an created board</returns>
+        public char[] createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                Console.WriteLine(board[i] = '_');
+            }
+            return board;
+        }
+    }
+}
